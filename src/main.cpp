266ad73d2d13274c9becc558bc7dcd10e368c924
henrymaquera09 <hmmmamani@unjbg.edu.pe>
2025-10-12 21:54:36 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "command.h"
+#include "parser.h"
+#include "executor.h"
+using namespace std;
+void show_prompt(){
+  cout<<"mi_shell";
+  cout.flush();
+}
 
+int main(){
+  string line;
+  while(true){
+    show_prompt();
+    if()
+  }
+  return 0;
+}
