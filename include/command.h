60e4cf1b073135e42f5e2c08f1c35c9c225eae4b
henrymaquera09 <hmmmamani@unjbg.edu.pe>
2025-10-12 22:01:33 -0500

    Update and rename comando.h to command.h

---
+++ include/command.h
@@ -0,0 +1,22 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+#include<string>
+#include<vector>
+using namespace std;
+enum class RedirectionType{
+  NONE,
+  TRUNCATE,
+  APPEND
+};
+struct SimpleCommand{
+  string executable;
+  vector<string> arguments;
+};
+struct Command{
+  vector<SimpleCommand> commands;
+  string inputFile;
+  string outputFile;
+  RedirectionType rendirection = RedirectionType::NONE;
+  bool background = false;
+};
+#endif
